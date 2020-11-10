276240ff5b1c8f2b81dbf0e5cd642648e80ea958
Kristiyan <parlikov0912@gmail.com>
2020-11-10 16:28:01 +0100

    Swapping the previous API project with an API with authentication

--- RESTWebAPI/Properties/AssemblyInfo.cs
+++ RESTWebAPI/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e5fcfc65-fb90-40af-844e-10aeaec4117e")]
+[assembly: Guid("0f90fcb4-0f07-4c42-b9cd-54d7e0f01735")]
 
 // Version information for an assembly consists of the following four values:
 //
