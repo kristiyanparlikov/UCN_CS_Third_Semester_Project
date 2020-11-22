43217a9c24f75e935a2df03e0503b6c1e62b31f1
alanismirko <61684223+alanismirko@users.noreply.github.com>
2020-11-22 21:04:37 +0100

    Adding DormitoryModel and StudentHandler

--- BusinessLayer/StudentHandler.cs
+++ BusinessLayer/StudentHandler.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using DataAccessLayer;
+using DataAccessLayer.Repository;
+using ModelLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UCNThirdSemesterProject.ModelLayer;
 
 namespace BusinessLayer
 {
-    class StudentHandler
+    class StudentHandler : ICRUD<StudentModel>
     {
+        IStudentRepository db = new StudentRepository();
+
+        public void Create(StudentModel entity)
+        {
+            db.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            db.Remove(id);
+        }
+
+        public StudentModel Get(int id)
+        {
+            return db.Find(id);
+        }
+
+        public IEnumerable<StudentModel> GetAll()
+        {
+            return db.GetAll();
+        }
+
+        public void Update(StudentModel entity)
+        {
+            //db.Update(entity.Id);
+        }
     }
 }
---
+++ ModelLayer/DormitoryModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public class DormitoryModel
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int NDayNotice { get; set; }
+
+        public DormitoryModel(int id, string name, int nDayNotice)
+        {
+            Id = id;
+            Name = name;
+            NDayNotice = nDayNotice;
+        }
+
+        public DormitoryModel(string name, int nDayNotice)
+        {
+            Name = name;
+            NDayNotice = nDayNotice;
+        }
+    }
+}
