0740283e3dc8650fa667a005b04e3781d1fb0ee9
alanismirko <61684223+alanismirko@users.noreply.github.com>
2020-11-22 21:29:52 +0100

    Changes to StudentModel

--- RESTWebAPI/Models/Student.cs
+++ RESTWebAPI/Models/Student.cs
@@ -9,18 +9,27 @@ namespace RESTWebAPI.Models
 {
     public class Student : User
     {
+       
         public string DateOfBirth { get; set; }
         public string EducationEndDate { get; set; }
         public string Nationality { get; set; } 
 
 
 
-        public Student(int id, string firstName, string lastName, string phoneNumber, string email, string dateOfBirth, string educationEndDate, string nationality): base(id, firstName, lastName, phoneNumber,email)
+        public Student(int id,string firstName, string lastName, string phoneNumber, string email, string dateOfBirth, string educationEndDate, string nationality): base(id,firstName, lastName, phoneNumber,email)
         {
             DateOfBirth = dateOfBirth;
             EducationEndDate = educationEndDate;
             Nationality = nationality;
                 
+        }
+
+        public Student( string firstName, string lastName, string phoneNumber, string email, string dateOfBirth, string educationEndDate, string nationality) : base(firstName, lastName, phoneNumber, email)
+
+        {
+            DateOfBirth = dateOfBirth;
+            EducationEndDate = educationEndDate;
+            Nationality = nationality;
         }
     }
 }
