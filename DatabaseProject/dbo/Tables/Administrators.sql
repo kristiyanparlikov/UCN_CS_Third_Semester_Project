1d64423827ba9eeeff3f7445cf611bea466cbb6b
Kristiyan <parlikov0912@gmail.com>
2020-11-28 22:04:28 +0100

    Changed tables- adding password field

--- DatabaseProject/dbo/Tables/Administrators.sql
+++ DatabaseProject/dbo/Tables/Administrators.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[Administrators] (
     [Id]             INT            IDENTITY (1, 1) NOT NULL,
-    [AuthId]         NVARCHAR (128) ,
+    [Email]          NVARCHAR (256) NOT NULL,
+    [Password]         NVARCHAR (256) NOT NULL,
     [EmployeeNumber] NVARCHAR (50)  NOT NULL,
     [FirstName]      NVARCHAR (50)  NOT NULL,
     [LastName]       NVARCHAR (50)  NOT NULL,
     [PhoneNumber]    NVARCHAR (15)  NOT NULL,
-    [Email]          NVARCHAR (256) NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
--- DatabaseProject/dbo/Tables/Students.sql
+++ DatabaseProject/dbo/Tables/Students.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE [dbo].[Students] (
     [Id]               INT            IDENTITY (1, 1) NOT NULL,
-    [AuthId]           NVARCHAR (128) ,
+    [Email]            NVARCHAR (256) NOT NULL,
+    [Password]         NVARCHAR (256) NOT NULL,
     [FirstName]        NVARCHAR (50)  NOT NULL,
     [LastName]         NVARCHAR (50)  NOT NULL,
     [PhoneNumber]      NVARCHAR (15)  NOT NULL,
-    [Email]            NVARCHAR (256) NOT NULL,
     [DateOfBirth]      DATE           NOT NULL,
     [Nationality]      NVARCHAR (50)  NOT NULL,
     [EducationEndDate] DATE           NOT NULL,
-    [AddressId]        INT            NOT NULL,
+    [AddressId]        INT            ,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
