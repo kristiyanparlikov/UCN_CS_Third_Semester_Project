15a2f9b0598655804e6ed657b15e1c5857101289
alanismirko <61684223+alanismirko@users.noreply.github.com>
2020-12-02 22:01:44 +0100

    Search method for the rooms table

--- WebApplication/Controllers/RoomsController.cs
+++ WebApplication/Controllers/RoomsController.cs
@@ -17,8 +17,8 @@ namespace WebApplication.Controllers
 
         static List<RoomModel> rooms = new List<RoomModel>();
 
-        // GET: Rooms
-        public async Task<ActionResult> Rooms()
+        // GET: Rooms with a search filter attribute
+        public async Task<ActionResult> Rooms(string searchString)
         {
             using (var client = new HttpClient())
             {
@@ -40,11 +40,17 @@ namespace WebApplication.Controllers
 
                     //Deserializing the response recieved from web api and storing into the Employee list  
                     rooms = JsonConvert.DeserializeObject<List<RoomModel>>(roomsResponse);
+
+                    // The rooms array is filtered with the search string if the string is not null, empty or whitespace
+                    if (!string.IsNullOrEmpty(searchString) && !string.IsNullOrWhiteSpace(searchString))
+                        rooms = rooms.FindAll(room => room.Price.ToString().Equals(searchString));
                 }
             }
             //returning the rooms list to view 
             return View(rooms);
         }
+
+        
         public ActionResult Room(int id)
         {
             var room = rooms.Where(r => r.Id == id).FirstOrDefault();
@@ -77,5 +83,51 @@ namespace WebApplication.Controllers
             //return View(room);
         }
 
+        /*
+        [HttpPost]
+        public ActionResult Filter(string SelectedPrice, string SelectedCapacity)
+        {
+
+            var rawData = (from r in rooms
+                           select r);
+
+            var room = from r in rawData
+                       select r;
+
+            if (!String.IsNullOrEmpty(SelectedPrice))
+            {
+                room = room.Where(r => r.Price.ToString().Trim().Equals(SelectedPrice.Trim()));
+
+            }
+            if (!String.IsNullOrEmpty(SelectedCapacity))
+            {
+                room = room.Where(r => r.Price.ToString().Trim().Equals(SelectedCapacity.Trim()));
+
+            }
+
+            var UniquePrice = from r in room
+                              group r by r.Price into newGroup
+                              where newGroup.Key != null
+                              orderby newGroup.Key
+                              select new { Price = newGroup.Key };
+            ViewBag.UniquePrice = UniquePrice.Select(m => new SelectListItem { Value = m.Price.ToString(), Text = m.Price.ToString() }).ToList();
+
+            var UniqueCapacity = from r in room
+                                 group r by r.Capacity into newGroup
+                                 where newGroup.Key != null
+                                 orderby newGroup.Key
+                                 select new { Capacity = newGroup.Key };
+            ViewBag.UniqueCapacity = UniqueCapacity.Select(m => new SelectListItem { Value = m.Capacity.ToString(), Text = m.Capacity.ToString() }).ToList();
+
+            ViewBag.SelectedPrice = SelectedPrice;
+            ViewBag.SelectedCapacity = SelectedCapacity;
+
+            return View(room);
+        */
+
     }
-}
+
+}    
+
+
+    
